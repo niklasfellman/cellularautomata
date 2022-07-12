169e1b8fcad48993de08a1a615c7976c5a31e639
niklasfellman <purjotti@gmail.com>
2022-07-12 19:56:39 +0300

    game of life working

--- game-of-life/script.js
+++ game-of-life/script.js
@@ -2,11 +2,11 @@ const startBtn = document.querySelector(".start-btn")
 
 const canvas = document.querySelector("#canvas")
 const c = canvas.getContext("2d")
-let width = 600;
-let height = 600;
+let width = 1000;
+let height = 1000;
 canvas.width = width
 canvas.height = height
-let resolution = 10
+let resolution = 200
 
 class Cell{
 	constructor(x,y){
@@ -14,22 +14,37 @@ class Cell{
 		this.y = y
 		this.alive = Math.random() > .5 ? true:false
 		this.previous = this.alive
-		this.neighbors = 0	
+		this.neighbors = 0
+		this.next = true	
 	}
 	
+		
+
 	display(){
 		c.fillStyle = this.alive ? "white" : "black"
 		c.fillRect(this.x*width/resolution,this.y*height/resolution,width/resolution,height/resolution)	
-		c.fillStyle = "red"
-		c.font = "12px Arial"
-		c.fillText(this.neighbors,this.x*width / resolution + height/resolution/2 - 3,(this.y*height /resolution) + (height/resolution)/2 +5)
+		//c.fillStyle = "red"
+		//c.font = "12px Arial"
+		//c.fillText(this.neighbors,this.x*width / resolution + height/resolution/2 - 3,(this.y*height /resolution) + (height/resolution)/2 +5)
+		this.deadOrAlive()
 	}
 
-	updateNieghbors(x){
+	updateNeighbors(x){
 		if(this.alive){x = x-1}
 		this.neighbors = x
 	}
-	
+
+	deadOrAlive(){
+		if(!this.alive && this.neighbors === 3){
+			this.next = true
+		}
+		if(this.alive && this.neighbors === 2 || this.neighbors === 3){
+			this.next = true
+		}
+		else{
+			this.next = false
+		}
+	}	
 	
 	
 
@@ -53,7 +68,7 @@ let previousTime
 let animationID
 function animate(time){
 	animationID = requestAnimationFrame(animate)
-	if(time - previousTime < 500){
+	if(time - previousTime < 1){
 		return
 	}
 	previousTime = time
@@ -61,10 +76,26 @@ function animate(time){
 
 	for(let i = 0;i<boardArr.length;i++){
 		for(let j = 0;j<boardArr[i].length;j++){
-			boardArr[i][j].alive = !boardArr[i][j].alive
+			let count = 0
+			for(let k = -1;k<2;k++){
+				for(let l  = -1;l<2;l++){
+					let current = boardArr[(i+k+resolution)%resolution][(j+l + resolution)%resolution]
+					if(current.alive){
+						count ++	
+					}
+				}
+			}	
+			boardArr[i][j].updateNeighbors(count)
 			boardArr[i][j].display()
 		}
 	}
+
+	for(let i = 0;i<boardArr.length;i++){
+		for(let j = 0;j<boardArr[i].length;j++){
+			boardArr[i][j].alive = boardArr[i][j].next
+		}
+	}
+
 }
 
 function stopAnimate(){
