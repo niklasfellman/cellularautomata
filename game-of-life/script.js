be30f7a03355c72dcc878e670ea77f9e0ad55cb0
niklasfellman <purjotti@gmail.com>
2022-07-14 18:28:25 +0300

    added aspect ratio

--- game-of-life/script.js
+++ game-of-life/script.js
@@ -1,17 +1,19 @@
 const startBtn = document.querySelector(".start-btn")
 const canvas = document.querySelector("#canvas")
 const c = canvas.getContext("2d")
-let width = 950;
-let height = 950;
+
+let height = 600;
+let widthToHeightRatio = 4
+let width = height * widthToHeightRatio 
 canvas.width = width
 canvas.height = height
-let resolution = 200
+let resolution = 100
 
 class Cell{
 	constructor(x,y){
 		this.x = x
 		this.y = y
-		this.alive = Math.random() > .5 ? true:false
+		this.alive = Math.random() * Math.random() > .5 ? true:false
 		this.previous = this.alive
 		this.neighbors = 0
 		this.next = true	
@@ -19,14 +21,9 @@ class Cell{
 	}
 
 	display(){
-		//c.fillStyle = this.alive ? "white" : "black"
-		c.fillStyle = `hsl(20,${this.timeSinceAlive }%,${100 - this.timeSinceAlive / 2}%)`
-		c.fillRect(this.x*width/resolution,this.y*height/resolution,width/resolution,height/resolution)	
-// =>=>=>=> FOR TROUBLESHOOTING NEIGHBORS <=<=<=<=<=
-		//c.fillStyle = "red"
-		//c.font = "12px Arial"
-		//c.fillText(this.neighbors,this.x*width / resolution + height/resolution/2 - 3,(this.y*height /resolution) + (height/resolution)/2 +5)
-// ===================================================================================================================================================
+	//	c.fillStyle = this.alive ? "white" : "black"
+		c.fillStyle = `hsl(20,${this.timeSinceAlive * 2 }%,${100 - this.timeSinceAlive * 1 }%)`
+		c.fillRect(this.x * (width/(resolution*(width/height))),this.y*height/resolution,height/resolution,height/resolution)	
 		this.deadOrAlive()
 		this.updateTimeAlive()
 	}
@@ -37,9 +34,6 @@ class Cell{
 	}
 
 	deadOrAlive(){
-		if(!this.alive && this.neighbors === 3){
-			this.next = true
-		}
 		if(this.alive && this.neighbors === 2 || this.neighbors === 3){
 			this.next = true
 		}
@@ -56,12 +50,11 @@ class Cell{
 		this.timeSinceAlive = 0
 		}
 	}
-	
 }
 
 let boardArr = []
 
-for(let i = 0;i<resolution;i++){
+for(let i = 0;i<resolution * (width / height);i++){
 	boardArr.push([])
 	for(let j = 0;j<resolution;j++){
 		boardArr[i].push(new Cell(i,j))
@@ -71,6 +64,10 @@ for(let i = 0;i<resolution;i++){
 
 console.log(boardArr)
 
+
+let columns = resolution * widthToHeightRatio
+let rows = resolution
+ 
 let previousTime
 let animationID
 function animate(time){
@@ -83,26 +80,26 @@ function animate(time){
 	for(let i = 0;i<boardArr.length;i++){
 		for(let j = 0;j<boardArr[i].length;j++){
 			let count = 0
+// =>=>=>=>=>=>=>=>=>=>=>=>=> CHECK FOR NEIGHBORS <=<=<=<=<=<=<=<=<=<=<=<=<=<=
 			for(let k = -1;k<2;k++){
 				for(let l  = -1;l<2;l++){
-					let current = boardArr[(i+k+resolution)%resolution][(j+l + resolution)%resolution]
+					let current = boardArr[(i+k+columns)%columns][(j+l + rows)%rows]
 					if(current.alive){
 						count ++	
 					}
 				}
 			}	
+// =>=>=>=>=>=>=>=>=>=>=>=>=> =================== <=<=<=<=<=<=<=<=<=<=<=<=<=<=
 			boardArr[i][j].updateNeighbors(count)
 			boardArr[i][j].display()
 		}
 	}
-
 // =>=>=>=>=>=>=>=>=> WANT TO TRY TO GET THIS INTO PREVIOUS LOOP <=<=<=<=<=<=<=<=
 	for(let i = 0;i<boardArr.length;i++){
 		for(let j = 0;j<boardArr[i].length;j++){
 			boardArr[i][j].alive = boardArr[i][j].next
 		}
 	}
-
 }
 
 function stopAnimate(){
